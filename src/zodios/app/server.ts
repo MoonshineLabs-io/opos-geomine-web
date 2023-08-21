9411fbb1112fdbdbcd6220dcf0936b80fb47d293
Will Stone <willstone@gmail.com>
2023-08-21 01:03:26 -0400

    chore: disable x-powered-by

--- src/zodios/app/server.ts
+++ src/zodios/app/server.ts
@@ -7,6 +7,9 @@ import { specRouter } from "../api/spec";
 import { registerRouter } from "../api/register/registerRouter";
 
 const app = ctx.nextApp();
+// Disable the "X-Powered-By: Express" HTTP header for security reasons.
+// See https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
+app.disable("x-powered-by");
 app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
